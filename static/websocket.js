fdab78f145ac60b54018c3994877c259d5452058
Pascal <netpascal0123@aol.com>
2019-06-10 21:27:39 +0200

    CSS loader to wait for the generation

--- static/websocket.js
+++ static/websocket.js
@@ -1,5 +1,16 @@
 (function websocket () {
     var socket = new WebSocket(window.location.toString().replace("http", "ws"));
+    socket.addEventListener('open', function () {
+        HTMLElement.prototype.addAttributes = function(properties) {
+            Object.keys(properties).forEach(function(key) {
+                this[key] = properties[key];
+            }.bind(this));
+            return this
+        }
+        document.body.innerHTML = document.createElement("div").addAttributes({'className':"loader"}).outerHTML;
+        document.head.appendChild(document.createElement('link').addAttributes({'type':'text/css', 'rel':'stylesheet', 'href':'websocket.css'}));
+        document.title = "Loading..."
+    });
     socket.addEventListener('error', function (message) {
         console.log(["error", this, message]);
     });
